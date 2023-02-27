9f0dffea02af1a3e35e274849b353ca556e1d6d7
Pallavi Garg <pallavigarg101@gmail.com>
2023-02-27 15:41:04 -0800

    Copy output on clipboard

--- compiler.py
+++ compiler.py
@@ -4,6 +4,13 @@ from main_project.file_reader import File_Reader
 import argparse
 from main_project.dot_graph import Dot_Graph as dot
 
+from subprocess import Popen, PIPE
+
+def copy_clipboard(msg):
+    ''' Copy `msg` to the clipboard '''
+    with Popen(['xclip','-selection', 'clipboard'], stdin=PIPE) as pipe:
+        pipe.communicate(input=msg.encode('utf-8'))
+
 def print_warnings(parser):
     warnings = parser.warnings
     if len(warnings) > 0:
@@ -13,19 +20,24 @@ def print_warnings(parser):
         print("---------------------\n")
 
 def compile():
-
+    
     arg_parser = argparse.ArgumentParser(description="Compiles code of smpl language.")
     arg_parser.add_argument("file_path", help="Path of the file to compile.")
     args = arg_parser.parse_args()
 
     reader = File_Reader(args.file_path)
+    
     p = Parser(reader.get_contents())
 
     try:
         control_flow_graph = p.parse()
         print_warnings(p)
         dot_graph = dot()
-        print(dot_graph.get_representation(control_flow_graph))
+        output = dot_graph.get_representation(control_flow_graph)
+        print(output)
+        
+        #Only supported on linux
+        copy_clipboard(output)
     
     except Exception as ex:
         print_warnings(p)
