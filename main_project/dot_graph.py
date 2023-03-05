b11a5c449805c3c4d1d2dc5147495e20e6d88132
Pallavi Garg <pallavigarg101@gmail.com>
2023-03-04 20:21:35 -0800

    Update graph colors and labels

--- main_project/dot_graph.py
+++ main_project/dot_graph.py
@@ -1,4 +1,5 @@
 from .cfg import Control_Flow_Graph as cfg
+from .intermediate_representation import IR_One_Operand
 
 class Dot_Graph:
 # class that creates dot graph presentation graph of given cfg
@@ -27,21 +28,27 @@ class Dot_Graph:
 
     def __traverse_node(self, node):
         self.__declarations.append(f"{node} [shape=record, label=\"<b>{node} | {self.__traverse_instructions(node)}\"];")
-        fallthrough_label = ""
+        fallthrough = f"[label=\" fall-through\", color=darkblue, fontcolor=darkblue]"
+        loop = f"[label=\" loop\", color=brown, fontcolor=brown]"
+        branch = f"[label=\" branch\", color=darkgreen, fontcolor=darkgreen]"
+
         if node.branch_block is not None:
             from_block = f"{node}"[2:]
             to_block = f"{node.branch_block}"[2:]
             if int(to_block) < int(from_block):
-                self.__relations.append(f"{node}:s -> {node.branch_block}:n [label=\"loop\", color=brown, fontcolor=brown];")
+                self.__relations.append(f"{node}:s -> {node.branch_block}:n {loop};")
             else:
-                self.__relations.append(f"{node}:s -> {node.branch_block} [label=\"branch\"];")
-            fallthrough_label = f"[label=\"fall-through\"]"
+                self.__relations.append(f"{node}:s -> {node.branch_block} {branch};")
             
         if node.get_dominator_block() is not None:
-            self.__relations.append(f"{node.get_dominator_block()}:b -> {node}:b [color=blue, style=dotted, label=\"dom\", fontcolor=blue]")
+            self.__relations.append(f"{node.get_dominator_block()}:b -> {node}:b [color=purple, style=dotted, label=\"dom\", fontcolor=purple]")
         
         if node.fall_through_block is not None:
-            self.__relations.append(f"{node}:s -> {node.fall_through_block} {fallthrough_label};")
+            if len(node.get_instructions()) > 0:
+                last_instruction = node.get_instructions()[-1]
+                if isinstance(last_instruction, IR_One_Operand) and last_instruction.operand == node.fall_through_block:
+                    fallthrough = branch
+            self.__relations.append(f"{node}:s -> {node.fall_through_block} {fallthrough};")
         
 
     def __traverse_instructions(self, node):
