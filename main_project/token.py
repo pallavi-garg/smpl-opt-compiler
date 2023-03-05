2c3618503f6904bce6262454546502f04c6f0f16
Pallavi Garg <pallavigarg101@gmail.com>
2023-03-05 00:29:57 -0800

    fix token length issue

--- main_project/token.py
+++ main_project/token.py
@@ -6,26 +6,23 @@ class Token:
         self.id = None
         # represents constant value
         self.val = None
+        
         # type is Token_Type
         self.type = token_type
+        
         match token_type:
             case Token_Type.Identifier | \
                  Token_Type.Fn_OutputNum | \
                  Token_Type.Fn_InputNum | \
                  Token_Type.Fn_OutputNewLine:
                 self.id = val
+                self.len = len(val)
             case Token_Type.Number:
                 self.val = int(val)
+                self.len = len(val)
+            case _:
+                self.len = len(token_type)
     
     def get_length(self):
         # returns length of token string
-        match self.type:
-            case Token_Type.Identifier | \
-                 Token_Type.Fn_OutputNum | \
-                 Token_Type.Fn_InputNum | \
-                 Token_Type.Fn_OutputNewLine:
-                return len(self.id)
-            case Token_Type.Number:
-                return len(str(self.val))
-            case _:
-                return len(self.type)
+        return self.len
