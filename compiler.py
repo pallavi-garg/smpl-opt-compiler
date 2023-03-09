57d01c5712e3fde617b97cf44b27583e1629dd14
Pallavi Garg <pallavigarg101@gmail.com>
2023-03-09 00:18:27 -0800

    remove dce

--- compiler.py
+++ compiler.py
@@ -33,8 +33,11 @@ def compile():
 
     try:
         control_flow_graph = p.parse()
+
+        '''
         dce = DE_Eliminator()
         dce.eliminate(control_flow_graph)
+        '''
 
         warnings = get_warnings(p)
         dot_graph = dot()
